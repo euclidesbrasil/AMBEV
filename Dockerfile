2634e8b2360af5a585d0e3b32a7930fdcbfa85ed
Euclides Brasil <euclides_brasil@hotmail.com>
2025-02-05 18:36:49 -0300

    feat(docker): config dockerfile and docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,21 @@ FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
 WORKDIR /app
 EXPOSE 80
 EXPOSE 443
-
+#
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 WORKDIR /src
-COPY ["Ambev.General.Api.csproj", "./Ambev.General.Api/"]
-RUN dotnet restore "./Ambev.General.Api/Ambev.General.Api.csproj"
-COPY . .
+#
+# Copia todos os arquivos para o contêiner
+COPY /src .
+RUN ls -la
+#
+# Restaura as dependências
+WORKDIR /src/Ambev.General.Api
+RUN dotnet restore "Ambev.General.Api.csproj"
+#
 WORKDIR "/src/Ambev.General.Api"
-RUN dotnet publish "Ambev.General.Api.csproj" -c Release -o /app/publish --no-restore
-
+RUN dotnet publish "Ambev.General.Api.csproj" --configuration Release -o /app/publish --no-restore
+#
 FROM base AS final
 WORKDIR /app
 COPY --from=build /app/publish .
--- src/Ambev.General.Api/Program.cs
+++ src/Ambev.General.Api/Program.cs
@@ -12,104 +12,108 @@ using Microsoft.EntityFrameworkCore;
 using Ambev.Infrastructure.Persistence.PostgreSQL.Seed;
 using Ambev.Core.Domain.Interfaces;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+public class Program
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "General Api - Users, Product, Sales and Cart", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    public static async Task Main(string[] args)
     {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter 'Bearer' [space] and then your token in the text input below. Example: \"Bearer 12345abcdef\""
-    });
+        var builder = WebApplication.CreateBuilder(args);
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
         {
-            new OpenApiSecurityScheme
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "General Api - Users, Product, Sales and Cart", Version = "v1" });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
-                Reference = new OpenApiReference
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Enter 'Bearer' [space] and then your token in the text input below. Example: \"Bearer 12345abcdef\""
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
                 {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
                 }
-            },
-            new string[] {}
-        }
-    });
-});
+            });
+        });
 
-//registro dos serviços
-builder.Services.AddInfrastructure(builder.Configuration);
-// Adicione o serviço de autenticação JWT
-var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
+        // Registro dos serviços
+        builder.Services.AddInfrastructure(builder.Configuration);
+
+        // Adicione o serviço de autenticação JWT
+        var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key))
+                };
+            });
+
+        // Registra o filtro de exceção personalizado
+        builder.Services.AddMvc(options =>
         {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings.Issuer,
-            ValidAudience = jwtSettings.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key))
-        };
-    });
+            options.Filters.Add(new CustomExceptionFilter());
+        });
 
+        var app = builder.Build();
 
-
-// Registra o filtro de exceção personalizado
-builder.Services.AddMvc(options =>
-{
-    options.Filters.Add(new CustomExceptionFilter());
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-// Use autenticação e autorização
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-
-// Instanciar banco mongodb
-using (var scope = app.Services.CreateScope())
-{
-    var database = scope.ServiceProvider.GetRequiredService<IMongoDatabase>();
-    var _jwtTokenService = scope.ServiceProvider.GetRequiredService<IJwtTokenService>();
-    var initializer = new MongoDbInitializer(database);
-    await initializer.InitializeAsync();
-
-    try
-    {
-        using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
-            context.Database.Migrate();
-            SeedData.Initialize(serviceScope.ServiceProvider, _jwtTokenService);
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        // Instanciar banco MongoDB
+        using (var scope = app.Services.CreateScope())
+        {
+            var database = scope.ServiceProvider.GetRequiredService<IMongoDatabase>();
+            var _jwtTokenService = scope.ServiceProvider.GetRequiredService<IJwtTokenService>();
+            var initializer = new MongoDbInitializer(database);
+            await initializer.InitializeAsync();
+
+            try
+            {
+                using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(serviceScope.ServiceProvider, _jwtTokenService);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        await app.RunAsync();
     }
 }
-app.Run();
